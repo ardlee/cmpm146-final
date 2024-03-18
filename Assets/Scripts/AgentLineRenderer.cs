a5a3465fc1285456ebe98f5ac7eb6cb0bb730bbb
Aaron-Vuong <81384460+Aaron-Vuong@users.noreply.github.com>
2024-03-18 03:22:19 -0700

    Adding GetPath function

--- Assets/Scripts/AgentLineRenderer.cs
+++ Assets/Scripts/AgentLineRenderer.cs
@@ -8,15 +8,32 @@ using UnityEngine.AI;
 public class AgentLineRenderer : MonoBehaviour
 {
     private LineRenderer line; //to hold the line Renderer
+    private NavMeshAgent agent;
+    public Transform goal;
+    public Color lineStartColor = Color.blue;
 
     private void Start()
     {
         line = GetComponent<LineRenderer>(); //get the line renderer
+        agent = GetComponent<NavMeshAgent>();
         line.material = new Material(Shader.Find("Sprites/Default"));
         // Set the start to a light blue.
-        line.startColor = Color.blue;
-    }
+        line.startColor = lineStartColor;
 
+        // Set the current goal as null
+        goal = null;
+    }
+    // Use this function for the player to draw the line but not move according to the agent.
+    public void getPath()
+    {
+        // Don't draw a path if we don't have a goal.
+        if (goal == null) { return; }
+        line.SetPosition(0, transform.position); //set the line's origin
+
+        agent.SetDestination(goal.position); //create the path
+        DrawPath(agent.path);
+        agent.isStopped = true; //Don't want to move the agent
+    }
     public void DrawPath(NavMeshPath path)
     {
         if (path.corners.Length < 2) //if the path has 1 or no corners, there is no need
